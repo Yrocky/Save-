0d8e5306a613b827b51248a64659e2ae0a0a4b8d
Yrocky <983272765@qq.com>
2016-06-17 16:32:10 +0800

    Initial Commit

---
+++ Save+/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Save+
+//
+//  Created by Youngrocky on 16/6/17.
+//  Copyright © 2016年 Rocky Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Save+/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Save+
+//
+//  Created by Youngrocky on 16/6/17.
+//  Copyright © 2016年 Rocky Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
